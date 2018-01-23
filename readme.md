ba3e664172673a641ebf447a574487b4d772a93f
Charles Hache <chache@cygnustech.ca>
2018-01-22 17:37:27 -0800

    Adding license and readme

---
+++ readme.md
@@ -0,0 +1,35 @@
+# ECWeatherDownloader
+
+Script to read and process CSV files from Environment Canada.
+
+## Setup
+
+Copy the weather_download_config_example.py file to weather_download_config.py.
+
+Edit the weather_download_config.py file as required.
+
+## Usage
+
+When executed, the script will download the current month's of data for each site listed in the configuration.  If the script is run in the first week of the month, it also downloads the previous month's data.
+
+Some processing is done to the raw Environment Canada data files.  In particular, the script also does the following:
+
+ - Removes 'future' rows.  The Environment Canada data files also have timestamped rows for the future.  These rows are removed.
+ - Adds the appropriate timezone formatting to the first column's timestamps.  If you configure the correct timezone in the configuration file (info is included within that file), the the timestamps in the output CSV will be correct and useful for integration in other systems.
+ 
+## License
+ECWeatherDownloader
+Copyright (C) 2017  Cygnus Technical Services Ltd.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
--- weather_download_config_example.py
+++ weather_download_config_example.py
@@ -1,4 +1,21 @@
 
+# This file is part of ECWeatherDownloader.
+#
+# ECWeatherDownloader is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ECWeatherDownloader is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with ECWeatherDownloader.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# Copyright 2017 Charles Hache (chache@cygnustech.ca)
+
 #  Special note for timezones:
 #  From https://en.wikipedia.org/wiki/Tz_database#Area
 #  "The special area of "Etc" is used for some administrative zones, 
@@ -22,10 +39,12 @@
 #  above for some relevant info.
 # outtimezone: (optional) The timezone to use in the output/downloaded file.
 #  If this argument is not used, the timezone of the output is whatever timezone
-#  environment canada used in the file.
+#  environment canada used in the file.  Note that even if this parameter is not
+#  used, that the output timezone still gets modified from the environment 
+#  canada data; in particular the appropriate timezone is added.
 
 class WeatherDownloaderConfig:
   sites = [
     {'site':41883, 'filename':'{year:04d}-{month:02d}-Inuvik-EnvironmentCanada.csv', 'timezone':'Etc/GMT+7', 'outtimezone':'UTC'},
-    {'site':48168, 'filename':'{year:04d}-{month:02d}-Whitehorse-EnvironmentCanada.csv', 'timezone':'Etc/GMT+8', 'outtimezone':'UTC'},
+    {'site':48168, 'filename':'{year:04d}-{month:02d}-Whitehorse-EnvironmentCanada.csv', 'timezone':'Etc/GMT+8'},
     ]
